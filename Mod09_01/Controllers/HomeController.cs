6b817fa832e34967e3631f73b5030079127f74ad
auxo86 <auxo86@gmail.com>
2017-06-01 19:12:02 +0800

    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line.     [System.Web.Script.Services.ScriptService]

---
+++ Mod09_01/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mod09_01.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Home/Index2
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+        // GET: Home/SelectorDemo
+        public ActionResult SelectorDemo()
+        {
+            return View();
+        }
+
+        // GET: Home/HoverDemo
+        public ActionResult HoverDemo()
+        {
+            return View();
+        }
+
+        //GET: Home/JsonData?id=123&name=John
+        //GET: Home/JsonData/123&name=John
+        public ActionResult JsonData(string id, string name)
+        {
+            if (Request.IsAjaxRequest())
+            {
+                var data = new
+                {
+                    empid = id,
+                    EmpName = name,
+                    Age = DateTime.Now.Millisecond
+                };
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
+            return View();
+        }
+
+        public ActionResult AjaxDemo()
+        {
+            return View();
+        }
+    }
+}
---
+++ Mod09_01/Scripts/JavaScript1.js
@@ -0,0 +1,3 @@
+﻿function clickme() {
+    alert("Hello!!");
+}
